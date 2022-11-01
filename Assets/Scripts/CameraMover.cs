f60241758e503e3d45afe5bd64492d6aa9734119
CodedCrow <114866135+CodedCrow@users.noreply.github.com>
2022-11-01 16:39:57 -0400

    added committed

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
+
+    [SerializeField] private GameObject[] CameraPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
+        if(CameraPos[0].activeSelf){
+
+            YouPosisMyPos(CameraPos[0])
+            if(CameraPos[1].activeSelf){
+                Debug.Log("I like the car more");
+            }
+        }
+
+    else if(CameraPos[1].activeSelf){
+            YouPosisMyPos(CameraPos[1])
+    }
+    else{
+        Debug.Log("No active position for camera. REEEEEE!!!");
+    }
+
+    }
+
+    void YouPosisMyPos(GameObject reference){
+
     }
 }
